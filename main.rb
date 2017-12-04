85c2b302e3e1fab7b09ef1365c233bae9fe1afc6
Samplasion <ercolino2004@gmail.com>
2017-12-04 16:48:45 +0100

    Main refined

--- main.rb
+++ main.rb
@@ -1,6 +1,16 @@
 def fromBl(bl) {
-  resMB = mb * 8
-  resGB = resMB * 1024
+  resMB = bl / 8
+  resGB = resMB / 1024
   return [resMB, resGB]
+}
+def fromMb(mb) {
+  resBL = mb * 8
+  resGB = mb / 1028
+  return [resBL, resGB]
+}
+def fromGb(gb) {
+  resBL = (gb * 1028) * 8
+  resMB = gb * 1028
+  return [resBL, resMB]
 }
 puts fromBl(24599)
