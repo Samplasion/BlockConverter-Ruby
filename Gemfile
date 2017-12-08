32660169c7aa2f8d191b2a0c4a1ad173743bb4c3
Samplasion <ercolino2004@gmail.com>
2017-12-08 13:21:09 +0100

    Project => Gem

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+repo_name = "Samplasion/BlockConverter-Ruby"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+# Specify your gem's dependencies in blockconverter.gemspec
+gemspec
---
+++ lib/blockconverter.rb
@@ -0,0 +1,29 @@
+require "blockconverter/version"
+
+module Blockconverter
+  def fromBl(bl)
+    resMB = bl / 8
+    resGB = resMB / 1024
+    return [resMB, resGB]
+  end
+  def fromMb(mb)
+    resBL = mb * 8
+    resGB = mb / 1028
+    return [resBL, resGB]
+  end
+  def fromGb(gb)
+    resBL = (gb * 1028) * 8
+    resMB = gb * 1028
+    return [resBL, resMB]
+  end
+  def self.get(num, todo)
+    case todo
+      when "bl"
+        puts fromBl(num)
+      when "mb"
+        puts fromMb(num)
+      when "gb"
+        puts fromGb(num)
+    end
+  end
+end
